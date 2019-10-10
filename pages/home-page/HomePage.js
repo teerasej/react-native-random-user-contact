9df0bd87c5c212700b1d3247fc9d4d041e1fc692
Teerasej <teerasej@gmail.com>
2019-10-10 22:51:40 +0700

    เรียกใช้ Action function เพื่อเริ่มการขอข้อมูลจาก web api หลังจากแสดงหน้า home page แล้ว

--- pages/home-page/HomePage.js
+++ pages/home-page/HomePage.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react'
 import { View } from 'react-native'
 import { connect } from 'react-redux'
 import { Content, List, ListItem, Text, Body, Button, Icon } from 'native-base';
+import actions from "../../redux/actions";
 
 export class HomePage extends Component {
     
     static navigationOptions = {
         title: 'Contacts'  
     };
+
+    componentDidMount() {
+        this.props.startGetUser();
+    }
 
     openDetail = () => {
         this.props.navigation.navigate('Detail');
@@ -30,8 +35,8 @@ const mapStateToProps = (state) => ({
     
 })
 
-const mapDispatchToProps = {
-    
-}
+const mapDispatchToProps = dispatch => ({
+    startGetUser: () => actions.startGetUser(dispatch)
+})
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomePage)
