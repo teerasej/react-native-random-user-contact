f1672dad3ca14698d6b2f781171ccfb2c07ab812
Teerasej <teerasej@gmail.com>
2020-11-16 13:22:51 +0700

    clear for start

--- pages/home-page/HomePage.js
+++ pages/home-page/HomePage.js
@@ -7,7 +7,7 @@ import actions from "../../redux/actions";
 export class HomePage extends Component {
 
     componentDidMount() {
-        this.props.startGetUser();
+        
     }
 
     openDetail = (user) => {
@@ -58,7 +58,7 @@ const mapStateToProps = (state) => ({
 })
 
 const mapDispatchToProps = dispatch => ({
-    startGetUser: () => actions.startGetUser(dispatch),
+    
     selectUser: (user) => dispatch(actions.selectUser(user))
 })
 
