f76f64c5a61ada6130c56c5e590925ab268560a8
Teerasej <teerasej@gmail.com>
2021-03-29 20:25:04 +0700

    use Axios

--- redux/action-start-get-user.js
+++ redux/action-start-get-user.js
@@ -1,11 +1,27 @@
 
 import { Types } from './actions'
+import axios from 'axios'
 
 export const startGetUser = async (dispatch) => {
     
     const url = 'https://randomuser.me/api/?results=50';
 
-    
-    
+    const response = await axios.get(url);
+
+    if (response && response.status === 200) {
+
+        const json = await response.data;
+
+        console.log('JSON:',json);
+        console.log('Accounts:', json.results);
+
+        dispatch({
+            type: Types.GET_USERS_SUCCESS,
+            payload: json.results
+        });
+
+    } else {
+        console.error(response.status, response);
+    }
     
 }
