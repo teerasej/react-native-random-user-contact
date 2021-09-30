e5007697e98e68edc9d05227f3384e19cb2beb37
Teerasej Jiraphatchandej <teerasej@gmail.com>
2021-09-30 21:28:59 +0700

    finish get user by calling fetch

--- redux/actions.js
+++ redux/actions.js
@@ -1,24 +1,39 @@
 
-import call from 'react-native-phone-call'
-
 export const actionTypes = {
-    GET_USERS_SUCCESS: 'GET_USERS_SUCCESS',
-    USER_SELECTED: 'USER_SELECTED'
+    GET_USERS_SUCCESS: 'GET_USERS_SUCCESS'
 }
 
 export const startGetUser = async (dispatch) => {
     const url = 'https://randomuser.me/api/?results=50';
 
-}
 
-export const createAction_UserSelected = (user) => {
-    return {
-        type: actionTypes.USER_SELECTED,
-        payload: user
+    const response = await fetch(url, {
+        // กำหนด method GET
+        method: 'GET',
+        // กำหนด header เพื่อให้รับ และส่งข้อมูลแบบ JSON
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    });
+
+    // ถ้า status 200 หมายถึง response ตอบกลับมาสมบูรณ์
+    if (response && response.status === 200) {
+
+        // แปลงข้อมูล JSON
+        const json = await response.json();
+
+        console.log('JSON:', json);
+        console.log('Accounts:', json.results);
+
+        // ส่งข้อมูล .results ที่ได้จาก Web API (เป็น Array) เข้า Redux
+        dispatch({
+            type: actionTypes.GET_USERS_SUCCESS,
+            payload: json.results
+        });
+
+    } else {
+        console.error(response.status, response);
     }
-}
 
-export const makeCall = (phoneNumber) => {
-    call({number: phoneNumber}).catch(console.error);
 }
-
